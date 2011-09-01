66d2f521c16c1dc53ab80ab54be68b13e2f98400
Dale Harvey <dale@arandomurl.com>
2011-09-01 13:40:01 +0100

    only show tags for active items

--- views/tags/map.js
+++ views/tags/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.tags) {
+  if (doc.type && doc.type === 'task' && doc.status === 'active' && doc.tags) {
     for (var tag in doc.tags) {
       if (doc.tags[tag] !== "") {
         emit([doc.tags[tag]], null)
